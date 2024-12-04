0b8cd8a57dc94617ea9132b2e03bceb8b4c7ab51
phillipbjj <pjdempsey3@gmail.com>
2024-12-04 11:03:30 -0600

    renamed html file, established homepage

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+#from pydantic import BaseModel
 
 app = FastAPI()
 
 #class Item (BaseModel):
-    
+#Mount the static directory to serve static files
+app.mount('/static', StaticFiles(directory='static'), name='static')
     
 @app.get("/")
 async def read_root():
-    return "Home page"
+    return FileResponse('static/index.html')
