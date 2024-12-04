eafa0664ac70c194a54e10675664d8d29ef4251b
phillipbjj <pjdempsey3@gmail.com>
2024-12-04 14:57:52 -0600

    set up jinja2 for html templates, correctly routing homepage

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+
+#from typing import Union
+#from starlette.responses import FileResponse
 #from pydantic import BaseModel
 
 app = FastAPI()
+#Mount the static directory to serve static files. Used for sub-apps or static files.
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="templates")
 
 #class Item (BaseModel):
-#Mount the static directory to serve static files
-app.mount('/static', StaticFiles(directory='static'), name='static')
-    
+
 @app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    
+"""@app.get("/")
 async def read_root():
-    return FileResponse('static/index.html')
+    return FileResponse('static/index.html')"""
