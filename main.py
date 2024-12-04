8641bd41305966d252ccf5fb74db219f277c6881
phillipbjj <pjdempsey3@gmail.com>
2024-12-04 10:34:07 -0600

    added html template homepage

--- main.py
+++ main.py
@@ -5,20 +5,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item (BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+#class Item (BaseModel):
+    
     
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return "Home page"
