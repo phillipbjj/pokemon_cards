235b60bdc80a8009c4c345f2139709291e982d36
phillipbjj <pjdempsey3@gmail.com>
2024-12-05 10:09:53 -0600

    switched to url_for() in html and added response_class for it

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 #from typing import Union
-#from starlette.responses import FileResponse
 #from pydantic import BaseModel
 
+#Create FastAPI application
 app = FastAPI()
 #Mount the static directory to serve static files. Used for sub-apps or static files.
 app.mount('/static', StaticFiles(directory='static'), name='static')
-
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
 #class Item (BaseModel):
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
     
