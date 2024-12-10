939b97078faf815ac2d3e3e23f804c68929db80c
phillipbjj <pjdempsey3@gmail.com>
2024-12-10 16:24:05 -0600

    added enums for user roles

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from enum import Enum
 #from typing import Union
 #from pydantic import BaseModel
 
+#Enum for static user roles
+class UserRoles(str, Enum):
+    admin = 'admin'
+    user = 'user'
+    shop_owner = 'shop_owner'
+    guest = 'guest'
+    
 #Create FastAPI application
 app = FastAPI(title='CardShop')
 #Mount the static directory to serve static files. Used for sub-apps or static files.
@@ -18,6 +26,14 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
     
-"""@app.get("/")
-async def read_root():
-    return FileResponse('static/index.html')"""
+@app.get("/users/register", response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/users/login", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
