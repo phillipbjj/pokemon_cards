431b2fce2c42f5778a5d6410e50d1384c79b5d60
phillipbjj <pjdempsey3@gmail.com>
2025-01-02 16:34:35 -0600

    testpoints

--- testpoints.py
+++ testpoints.py
@@ -11,41 +11,24 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
-#class Item (BaseModel):
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
     
+@app.get("/users/login", response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse("users_template.html", {"request": request}) 
 
+@app.get("/users/register", response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse("register_template.html", {"request": request})
 
-"""from fastapi import Depends, FastAPI, Form, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
-import psycopg2
-from psycopg2.extras import RealDictCursor
+@app.get("/users/account", response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse("users_template.html", {"request": request}) 
 
-app = FastAPI()
+@app.get("/cards", response_class=HTMLResponse)
+async def get_cards(request: Request):
+    return templates.TemplateResponse("cards_template.html", {"request": request}) 
 
-def get_db():
-    conn = psycopg2.connect("dbname=test user=postgres password=secret")
-    try:
-        yield conn
-    finally:
-        conn.close()
-
-def get_cursor(conn=Depends(get_db)):
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    try:
-        yield cursor
-    finally:
-        cursor.close()
-
-@app.post("/users/register", response_class=HTMLResponse)
-async def add_user(username: str = Form(...), password: str = Form(...), email: str = Form(...), cur=Depends(get_cursor)):
-    try:
-        cur.execute('''INSERT INTO users (username, password, email) VALUES (%s, %s, %s)''', (username, password, email))
-        cur.connection.commit()
-        return RedirectResponse("/", status_code=302)
-    except Exception as e:
-        cur.connection.rollback()
-        raise HTTPException(status_code=400, detail=str(e))"""
